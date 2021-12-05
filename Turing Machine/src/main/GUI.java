619ba1d16ad0374e128a816ca4ac093e32940eef
pinna <pinna9001@web.de>
2021-12-05 11:42:24 +0100

    update GUI

--- Turing Machine/src/main/GUI.java
+++ Turing Machine/src/main/GUI.java
@@ -1,59 +1,86 @@
 package main;
+
 import java.awt.Toolkit;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class GUI {
-	
+
 	private static Display display;
-	
+
 	private Shell shell;
-	
+
+	public Text editingField;
+
 	public GUI() {
 		initalizeGUI();
-		initalizeSimulatorWindow();
-		initalizeEditingTextField();
+		createGUIComponents();
+
 	}
-	
+
 	public void initalizeGUI() {
 		createDisplay();
 		createShell();
 	}
-	
-	public void initalizeSimulatorWindow() {
-		
+
+	public void createGUIComponents() {
+		createMenuBar();
+		createSimulatorWindow();
+		createEditingTextField();
 	}
-	
-	public void initalizeEditingTextField() {
-		
-	}
-	
+
 	public void openGUI() {
 		this.shell.pack();
 		this.shell.open();
-		
+
 		while (!this.shell.isDisposed()) {
-			if (!GUI.display.readAndDispatch()) 
+			if (!GUI.display.readAndDispatch())
 				GUI.display.sleep();
-		}	
+		}
 	}
-	
+
 	public void closeGUI() {
 		GUI.display.dispose();
 	}
-	
+
 	public void createDisplay() {
-		if(GUI.display==null) {
+		if (GUI.display == null) {
 			GUI.display = new Display();
 		}
 	}
-	
+
 	public void createShell() {
 		this.shell = new Shell(GUI.display);
-		this.shell.setMinimumSize(Toolkit.getDefaultToolkit().getScreenSize().width/2,Toolkit.getDefaultToolkit().getScreenSize().height/2);
+
+		int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+		int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+		this.shell.setMinimumSize(screenWidth >> 1, screenHeight >> 1);
 		this.shell.setText("Turing Machine");
-		this.shell.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/4,Toolkit.getDefaultToolkit().getScreenSize().height/4);
+		this.shell.setLocation(screenWidth >> 2, screenHeight >> 2);
+		this.shell.setLayout(new GridLayout(2, true));
+	}
+
+	public void createMenuBar() {
+		
+	}	
+	
+	public void createSimulatorWindow() {
+		Label label = new Label(shell, 0);
+		// TODO
+	}
+
+	public void createEditingTextField() {
+		editingField = new Text(shell, SWT.V_SCROLL | SWT.MULTI | SWT.WRAP);
+		editingField.setText("Test");
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		editingField.setLayoutData(layoutData);
 	}
 
 }
