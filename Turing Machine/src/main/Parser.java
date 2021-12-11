b105587b5dc08d52fbdb0ab5f2f63b99b7f381ab
mto99 <ozmen.muhammed@outlook.com>
2021-12-11 21:26:25 +0100

    main: implemented parser (neccessary for TuringMachine Simulation)

--- Turing Machine/src/main/Parser.java
+++ Turing Machine/src/main/Parser.java
@@ -1,22 +1,30 @@
 package main;
 
-import java.util.List;
+import java.io.FileReader;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
-import simulation.State;
 
 public class Parser {
 	
 	// Variables
-	private String alphabet;
-	private List<State> states;
-	private State startState;
-	private State acceptState;
-	private State rejectState;
-	private String State;
 	
-	// Constructor
-	public String parseAndValidate(String text) {
-	// TODO implement
-		return null;
+	
+	public JSONObject parseAndValidate(String filename) {
+		
+		JSONParser parser = new JSONParser();
+		
+		try {
+			Object obj = parser.parse(new FileReader(filename));
+			JSONObject jsonObject = (JSONObject)obj;			
+			return jsonObject;
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+		
+		
+		
 	}
 }
