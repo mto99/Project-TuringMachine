fffd9afe0883b6d382d522f4d2c03cddd8c2e211
mto99 <ozmen.muhammed@outlook.com>
2021-12-12 08:40:12 +0100

    Parser

--- Turing Machine/src/main/Parser.java
+++ Turing Machine/src/main/Parser.java
@@ -1,13 +1,24 @@
 package main;
 
 import java.io.FileReader;
+import java.util.List;
+
 import org.json.simple.*;
 import org.json.simple.parser.*;
+
+import simulation.State;
 
 
 public class Parser {
 	
 	// Variables
+	private String alphabet;
+	private List<State> states;
+	private State startState;
+	private State acceptState;
+	private State rejectState;
+	private String tape;
+	
 	
 	
 	public JSONObject parseAndValidate(String filename) {
